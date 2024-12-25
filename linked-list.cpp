741ee33a2f786be9ebebfc418bdd0d6a0c0bbe0c
Tran Manh Khiem <khiemmanhtran@gmail.com>
2024-12-25 14:49:12 +0700

    Tạo danh sách liên kết

--- linked-list.cpp
+++ linked-list.cpp
@@ -3,12 +3,50 @@
 using namespace std;
 
 struct Node {
-    int data ;
-    Node *next;
+    int data;
+    Node* next;
 };
 
-typedef struct Node* node;
+Node* createNode(int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = nullptr;
+    return newNode;
+}
+
+void insertAtTail(Node*& head, int value) {
+    Node* newNode = createNode(value);
+    if (head == nullptr) {
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head; // Node tạm thời để không thay đổi head
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+} 
+
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
 
 int main() {
-    cout << sizeof(Node);
+    Node* head = nullptr;
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        int value;
+        cin >> value;
+        insertAtTail(head, value);
+    }
+
+    printList(head);
 }
