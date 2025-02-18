a8c9b46ef62f605cf10fb8ffa60bd2010a8b1fa1
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-18 11:09:07 +0700

    SỐ LỘC PHÁT 1

--- DSA08017.cpp
+++ DSA08017.cpp
@@ -2,26 +2,28 @@
 
 using namespace std;
 
-bool compare(string a, string b) {
-    if (a.length() == b.length()) return a > b;
-    return a.length() > b.length();
-}
-void Try(vector<string> &v, string s, int n) {
-    if (s.size() == n + 1) return;
-    if (s.size() != 0) v.push_back(s);
-    Try(v, s + '6', n);
-    Try(v, s + '8', n);
-}
 int main() {
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
-        vector<string> v;
-        Try(v, "", n);
-        sort(v.begin(), v.end(), compare);
-        for (int i = 0; i < v.size(); i++) cout << v[i] << " ";
-        cout << endl;
+        queue<string> q;
+        q.push("6");
+        q.push("8");
+        
+        vector<string> res;
+        while (!q.empty()) {
+            string s = q.front();
+            q.pop();
+
+            if (s.size() > n) break;
+            res.push_back(s);
+            q.push(s + '6');
+            q.push(s + '8');
+        }
+
+        for (int i = res.size() - 1; i >= 0; i--) cout << res[i] << " ";
+        cout << endl; 
     }
 }
