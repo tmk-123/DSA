8396c70cb7e81f4bf2e2a0300b19e51bec44e5ce
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-03-20 19:23:36 +0700

    BIỂU DIỄN ĐỒ THỊ CÓ HƯỚNG.

---
+++ DSA09003.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+
+        unordered_map<int, vector<int>> adj;
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        for (int u = 1; u <= V; u++) {
+            cout << u << ": ";
+            for (auto& neighbor : adj[u]) {
+                cout << neighbor << " ";
+            }
+            cout << endl;
+        }
+    }
+}
