0cce773819d6826c94c427da5ff3c9681e933ead
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-05-29 23:44:14 +0700

    TẬP CON LIỀN KỀ PHÍA TRƯỚC

---
+++ DSA01018.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, k;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin >> n >> k;
+        vector<int> v(k);
+        for (int i = 1; i <= k; i++) {
+            cin >> v[i];
+        }  
+
+        int idx = k;
+        while (idx > 1 && v[idx] == v[idx - 1] + 1) {
+            idx--;
+        }
+
+        if (idx == 1 && v[idx] == 1) {
+            for (int i = 1; i <= k; i++) {
+                cout << n - k + i << " ";
+            }
+            cout << endl;
+        }
+        else {
+            v[idx]--;
+            for (int i = idx + 1; i <= k; i++) {
+                v[i] = n - k + i;
+            }
+
+            for (int i = 1; i <= k; i++) {
+                cout << v[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+}
