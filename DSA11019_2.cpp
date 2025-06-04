07a7140576efa3e5360753713789b92164a26c46
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-06-04 23:47:15 +0700

    DUYỆT CÂY NHỊ PHÂN TÌM KIẾM 2

---
+++ DSA11019_2.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int val;
+    Node *left, *right;
+    Node(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+Node* insert(Node* root, int n) {
+    if (!root) return new Node(n);
+
+    if (root->val < n) root->right = insert(root->right, n);
+    else root->left = insert(root->left, n);
+
+    return root;
+}
+
+void postOrder(Node* root) {
+    if (!root) return;
+    
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->val << " ";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Node* root = nullptr;
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            root = insert(root, x);
+        }
+
+        postOrder(root);
+        cout << endl;        
+    }
+}
